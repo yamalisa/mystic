a20f3f6ddcdd24013c64077160bc080cf358e378
yamalisa <168636012+yamalisa@users.noreply.github.com>
2025-08-18 02:32:38 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,3 +16,48 @@
      });
 
 }
+
+{
+    const next = document.getElementById('next');
+    const prev = document.getElementById('prev');
+    const ul = document.querySelector('.container2 ul');
+    const slides = ul.children;
+    let currentIndex = 0;
+
+    function updateButtons() {
+     prev.classList.remove('hidden');
+     next.classList.remove('hidden');
+
+      if (currentIndex === 0) {
+        prev.classList.add('hidden');
+      }
+     if (currentIndex === slides.length - 1) {
+        next.classList.add('hidden');
+      }
+    }
+
+    function moveSlides() {
+      const slideWidth = slides[0].getBoundingClientRect().width;
+    ul.style.transform = `translateX(${-1 * slideWidth * currentIndex}px)`;
+    }
+
+    updateButtons();
+
+
+  next.addEventListener('click', () => {
+    currentIndex++;
+    updateButtons();
+    moveSlides();
+  });
+
+  prev.addEventListener('click', () => {
+    currentIndex--;
+    updateButtons();
+    moveSlides();
+  });
+
+  window.addEventListener('resize', () => {
+   moveSlides();
+});
+}
+
